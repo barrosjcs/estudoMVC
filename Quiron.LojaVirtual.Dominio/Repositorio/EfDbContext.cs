b3220eb5e290aab07f368e4a9ec6469c33cbf46c
barrosjcs <je_caetano@hotmail.com>
2016-08-18 23:36:20 -0300

    Conexão a base e Entity Framework

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Remove Pluralização automática do Entity Framework        
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+}
