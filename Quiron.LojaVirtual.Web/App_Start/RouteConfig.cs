797dbc54e6b5e7aa6ff776dfd054e2bb677ff99f
barrosjcs <je_caetano@hotmail.com>
2016-08-21 22:33:15 -0300

    Roteamento amigável. Bootstrap

--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -12,11 +12,20 @@ namespace Quiron.LojaVirtual.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /* Roteamento definido com seu controller e action(view). Assim não mostrará estas informações na Url.
+               Se tornando uma visualização mais amigável.*/
+
+            routes.MapRoute(
+                name: null,
+                //Padrão que a Url terá. Entre chaves é o parâmetro da action, neste caso, é da ProdutosViewModel
+                url: "Pagina{pagina}",
+                defaults: new { controller = "Vitrine", action = "ListaProdutos" }
+                );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vitrine", action = "ListaProdutos", id = UrlParameter.Optional }
             );
         }
     }
