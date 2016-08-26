6b5f29e68d14aea11664627abb486eb375d89f48
barrosjcs <je_caetano@hotmail.com>
2016-08-25 23:58:20 -0300

    Criação da entidade Carrinho

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Carrinho
+    {
+        private readonly List<ItemCarrinho> itensCarrinho = new List<ItemCarrinho>();
+        public void AdicionarItem(Produto produto, int quantidade)
+        {
+            ItemCarrinho itemCarrinho = itensCarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
+
+            if(itemCarrinho == null)
+            {
+                itensCarrinho.Add(new ItemCarrinho
+                {
+                    Produto = produto,
+                    Quantidade = quantidade
+                });
+            }
+            else
+            {
+                itemCarrinho.Quantidade += quantidade;
+            }
+        }
+
+        public void RemoverItem(Produto produto)
+        {
+            itensCarrinho.RemoveAll(p => p.Produto.ProdutoId == produto.ProdutoId);
+        }
+
+        public decimal ObterValorTotal()
+        {
+            return itensCarrinho.Sum(p => p.Produto.Preco * p.Quantidade);
+        }
+
+        public void LimparCarrinho()
+        {
+            itensCarrinho.Clear();
+        }
+
+        public IEnumerable<ItemCarrinho> ItensCarrinho
+        {
+            get { return itensCarrinho; }
+        }
+
+    }
+
+    public class ItemCarrinho
+    {
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
