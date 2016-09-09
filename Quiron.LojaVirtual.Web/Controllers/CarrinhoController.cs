010426f677de0c2f7fa4b766ef2b7f8b92759efa
barrosjcs <je_caetano@hotmail.com>
2016-09-08 23:13:45 -0300

    Finalizando pedido e envio de e-mail

--- Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -1,6 +1,7 @@
 ﻿using Quiron.LojaVirtual.Dominio.Entidades;
 using Quiron.LojaVirtual.Dominio.Repositorio;
 using Quiron.LojaVirtual.Web.Models;
+using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -68,6 +69,38 @@ namespace Quiron.LojaVirtual.Web.Controllers
         public ViewResult FecharPedido()
         {
             return View(new Pedido());
+        }
+
+        [HttpPost]
+        public ViewResult FecharPedido(Pedido pedido)
+        {
+            EmailConfiguracoes email = new EmailConfiguracoes
+            {
+                EscreverArquivo = bool.Parse(ConfigurationManager.AppSettings["Email.EscreverArquivo"] ?? "false")
+            };
+
+            EmailPedido emailPedido = new EmailPedido(email);
+
+            if(!CarrinhoAtual.ItensCarrinho.Any())
+            {
+                ModelState.AddModelError("", "Não foi possível concluir o pedido, seu carrinho está vazio!");
+            }
+
+            if(ModelState.IsValid)
+            {
+                emailPedido.ProcessarPedido(CarrinhoAtual, pedido);
+                CarrinhoAtual.LimparCarrinho();
+                return View("PedidoConcluido");
+            }
+            else
+            {
+                return View(pedido);
+            }
+        }
+
+        public ViewResult PedidoConcluido()
+        {
+            return View();
         }
     }
 }
