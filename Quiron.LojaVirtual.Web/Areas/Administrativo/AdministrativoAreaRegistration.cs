0f69fb5b3e6a362adf8a43610b61a822fd5d1412
barrosjcs <je_caetano@hotmail.com>
2016-09-11 23:16:09 -0300

    Administrativo - Produto

---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/AdministrativoAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo
+{
+    public class AdministrativoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Administrativo";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            /*Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers é a adição de um namespace para ser verificado
+             os Controllers, pois se tiver um controller com o mesmo nome na raiz do projeto dará erro*/
+            context.MapRoute(
+                "Administrativo_default",
+                "Administrativo/{controller}/{action}/{id}",
+                new { controller = "Produto", action = "Index", id = UrlParameter.Optional },
+                new[] { "Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers" }
+            );
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio repositorio = new ProdutosRepositorio();
+
+        // GET: Administrativo/Produto
+        public ActionResult Index()
+        {
+            var produtos = repositorio.Produtos;
+
+            return View(produtos);
+        }
+    }
+}
