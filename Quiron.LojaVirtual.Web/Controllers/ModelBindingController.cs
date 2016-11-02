f7b2320911c5d3b161a2e58ca05ec52166352c4a
barrosjcs <je_caetano@hotmail.com>
2016-11-02 19:53:20 -0200

    Model Binding

---
+++ Quiron.LojaVirtual.Web/Controllers/ModelBindingController.cs
@@ -0,0 +1,35 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        // GET: ModelBinding
+        public ActionResult Index()
+        {
+            return View(new Produto());
+        }
+
+        [HttpPost]
+        public ActionResult Editar()
+        {
+            var produto = new Produto();
+
+            /* O UpdateModel irá atualizar a entidade Produto automaticamente, passando para o objeto
+             * os valores do form com o id de mesmo nome da propriedade. Porém poder ocorrer erro se 
+             a entidade requerir alguma propriedade obrigatório e a View não passar */
+            //UpdateModel(produto);
+
+            /* O TryUpdateModel irá atualizar a entidade Produto automaticamente se possíevel, passando para o objeto
+             * os valores do form com o id de mesmo nome da propriedade. */
+            TryUpdateModel(produto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Infraestrutura/CarrinhoModelBinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Web.Infraestrutura
+{
+    public class CarrinhoModelBinder : IModelBinder
+    {
+        private const string SessionKey = "Carrinho";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Carrinho carrinho = null;
+
+            if (controllerContext.HttpContext.Session != null)
+                carrinho = (Carrinho)controllerContext.HttpContext.Session[SessionKey];
+
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+
+                if (controllerContext.HttpContext.Session != null)
+                    controllerContext.HttpContext.Session[SessionKey] = carrinho;
+            }
+
+            return carrinho;
+        }
+    }
+}
