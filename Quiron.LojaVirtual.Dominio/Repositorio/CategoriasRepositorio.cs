4a1c9ffaaa36c9cb1fea558af68f01cb210ab1cd
barrosjcs <je_caetano@hotmail.com>
2017-03-01 22:55:26 -0300

    Aula 65 - Produto Vitrine

--- Quiron.LojaVirtual.Dominio/Repositorio/CategoriasRepositorio.cs
+++ Quiron.LojaVirtual.Dominio/Repositorio/CategoriasRepositorio.cs
@@ -1,10 +1,7 @@
 ﻿using Quiron.LojaVirtual.Dominio.Entidades;
 using Quiron.LojaVirtual.Dominio.Entidades.Vitrine;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quiron.LojaVirtual.Dominio.Repositorio
 {
